23743b583c5da3660a99b67d6e2a37e5a0dc7eb0
Chea69 <icyburb@gmail.com>
2026-03-30 15:40:40 +0700

    finish part B

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
+import { OrdersModule } from 'src/orders/orders.module';
 
 @Module({
+  imports: [OrdersModule], // ❌ now module graph is circular too
   providers: [NotificationsService],
   exports: [NotificationsService],
 })
--- src/notifications/notifications.service.ts
+++ src/notifications/notifications.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { OrdersService } from 'src/orders/orders.service';
 
 @Injectable()
 export class NotificationsService {
+  constructor(private readonly ordersService: OrdersService) {} // ❌ creates a circle
+
   notify(event: string, payload: any) {
-    // For lab: just log
     console.log(`[NOTIFY] ${event}`, payload);
+
+    // Example: call OrdersService for extra info (fake)
+    // this.ordersService.deleteOrder(); // don't actually do it, just for illustration
+
     return { ok: true };
   }
 }

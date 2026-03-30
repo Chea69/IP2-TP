93bf666548b629f3bc489504a3ec310fcc338562
Chea69 <icyburb@gmail.com>
2026-03-30 14:53:32 +0700

    add notify Module & Service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ReceiptsModule } from './receipts/receipts.module';
 import { Receipt } from './database/entities/receipts.entity';
+import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { Receipt } from './database/entities/receipts.entity';
       synchronize: true,
     }),
     ReceiptsModule,
+    NotificationsModule,
   ],
 })
 export class AppModule {}
